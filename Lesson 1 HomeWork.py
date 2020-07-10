2456b25bf21d0ac6e22aebcdf53347f9f5ae98db
DzianisMa <67979598+DzianisMa@users.noreply.github.com>
2020-07-10 13:00:21 +0300

    Update Lesson 1 HomeWork.py

--- Lesson 1 HomeWork.py
+++ Lesson 1 HomeWork.py
@@ -1,45 +1,45 @@
-# FirstName = input("Введите ваше имя >>>")
-# LastName = input("Введите вашу фамилия >>>")
-# City = input("Введите город вашего рождения >>>")
-# Birthday = int(input("Введите дату вашего рождения >>>"))
-# print(FirstName,LastName,'вы родились в городе',City, Birthday,' числа')
+FirstName = input("Введите ваше имя >>>")
+LastName = input("Введите вашу фамилия >>>")
+City = input("Введите город вашего рождения >>>")
+Birthday = int(input("Введите дату вашего рождения >>>"))
+print(FirstName,LastName,'вы родились в городе',City, Birthday,' числа')
 
 
-# seconds = int(input("Введите время в секундах >>>"))
-# hours = seconds // 3600
-# minutes = (seconds-hours*3600) // 60
-# second = seconds % 60
-# print(hours,'часов',minutes,'минут',second,'секунд')
+seconds = int(input("Введите время в секундах >>>"))
+hours = seconds // 3600
+minutes = (seconds-hours*3600) // 60
+second = seconds % 60
+print(hours,'часов',minutes,'минут',second,'секунд')
 
 
-# number = int(input("Введите значене числа N >>>"))
-# result = number + (number*10+number) + (number*100+number*10+number)
-# print ('Сумма числа',number,'+',number,number,'+',number,number,number,'равна',result)
+number = int(input("Введите значене числа N >>>"))
+result = number + (number*10+number) + (number*100+number*10+number)
+print ('Сумма числа',number,'+',number,number,'+',number,number,number,'равна',result)
 
 
-# n = abs(int(input("Введите целое положительное число ")))
-# max = n % 10
-# while n >= 1:
-#    n = n // 10
-#    if n % 10 > max:
- #       max = n % 10
-#    if n > 9:
-#        continue
-#    else:
-#        print("Максимальное цифра в числе ", max)
-#        break
+n = abs(int(input("Введите целое положительное число ")))
+max = n % 10
+while n >= 1:
+    n = n // 10
+    if n % 10 > max:
+        max = n % 10
+    if n > 9:
+        continue
+    else:
+        print("Максимальное цифра в числе ", max)
+        break
 
 
-#profit = float(input("Введите выручку фирмы "))
-#costs = float(input("Введите издержки фирмы "))
-#if profit > costs:
-#    print(f"Фирма работает с прибылью. Рентабельность выручки составила {profit / costs:.2f}")
-#    workers = int(input("Введите количество сотрудников фирмы "))
-#    print(f"прибыль в расчете на одного сторудника сотавила {profit / workers:.2f}")
-#elif profit == costs:
-#    print("Фирма работает в ноль")
-#else:
-#    print("Фирма работает в убыток")
+profit = float(input("Введите выручку фирмы "))
+costs = float(input("Введите издержки фирмы "))
+if profit > costs:
+    print(f"Фирма работает с прибылью. Рентабельность выручки составила {profit / costs:.2f}")
+    workers = int(input("Введите количество сотрудников фирмы "))
+    print(f"прибыль в расчете на одного сторудника сотавила {profit / workers:.2f}")
+elif profit == costs:
+    print("Фирма работает в ноль")
+else:
+    print("Фирма работает в убыток")
 
 
 a = int(input("Введите результаты пробежки первого дня в км "))
